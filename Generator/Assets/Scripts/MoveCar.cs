d3c1ef3f13c22b2b24e6515cdd5789873c048872
Buzzon <buzzondev@gmail.com>
2019-04-04 01:27:05 +0500

    ++

---
+++ Generator/Assets/Scripts/MoveCar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCar : MonoBehaviour
+{
+    private float speed = 0.5f;
+    Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 velocity = (transform.forward * vertical) * speed;
+        rigidbody.MovePosition(transform.position + velocity);
+
+        rigidbody.MoveRotation(transform.rotation * Quaternion.Euler(0f, horizontal * vertical, 0f));
+    }
+}

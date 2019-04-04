159d7f85cf10a6ec935223c8c4955faecf3e675e
SeGaRFromHeLL <sergeytrost@gmail.com>
2019-04-04 16:39:33 +0500

    DetailPrefabs

--- Generator/Assets/Scripts/Generate2.cs
+++ Generator/Assets/Scripts/Generate2.cs
@@ -2,20 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Generate2 : MonoBehaviour
 {
-    public GameObject platform;
-    public GameObject wheel;
-    public GameObject cabin;
+    [SerializeField]
+    private DetailPrefabs detailPrefabs;
 
     void Start()
     {
         Random.InitState((int)System.DateTime.Now.Ticks);
-        Car car = new Car(platform, transform);
-        car.Generate(new GameObject[] { platform, wheel, cabin });
-        
+        Car car = new Car(detailPrefabs, transform);
+        car.Generate();
+        int type = 0;
+        var field = typeof(DetailPrefabs).GetField(((DetailType)type).ToString());
+        field.GetValue(detailPrefabs);
+
 
     }
+}
+
+[System.Serializable]
+public struct DetailPrefabs
+{
+    public GameObject Platform;
+    public GameObject Wheel;
+    public GameObject Cabin;
 }
 
 public class Detail : MonoBehaviour
@@ -23,35 +34,49 @@ public class Detail : MonoBehaviour
     public DetailType Type;
     public Direction Direction;
     private GameObject detailObject;
+    private Detail[] Next;
 
     public Detail(GameObject _detail_object, Vector3 _position, Quaternion _rotation, Transform _parent, Direction _direction, DetailType _type)
     {
         detailObject = Instantiate(_detail_object, _position, _rotation, _parent);
         Direction = _direction;
         Type = _type;
+        Next = new Detail[(int)Direction.length];
+    }
+
+    public void Generate(DetailPrefabs detailPrefabs)
+    {
+        int num = Random.Range((Direction == Direction.Null) ? 2 : 1, (int)Direction.length);
+        for (int i = 0; i < num; i++)
+        {
+            int type = Random.Range(0, (int)DetailType.length);
+            var field = typeof(DetailPrefabs).GetField(((DetailType)type).ToString());
+            field.GetValue(detailPrefabs);
+        }
     }
 }
 
 public class Car : MonoBehaviour
 {
-    private List<Detail> details;
+    private Detail Head;
     private Probabilities probabilities;
+    private DetailPrefabs detailPrefabs;
 
-    public Car(GameObject platformPrefab, Transform transform)
+    public Car(DetailPrefabs _detailPrefabs, Transform _transform)
     {
-        details = new List<Detail> {
-            new Detail(platformPrefab, transform.position, transform.rotation, transform, Direction.Null, DetailType.Platfrom) };
+        detailPrefabs = _detailPrefabs;
+        Head = new Detail(detailPrefabs.Platform, _transform.position, _transform.rotation, _transform, Direction.Null, DetailType.Platfrom);
         probabilities = new Probabilities { Direction = new int[4], Detail = new int[byte.MaxValue] };
     }
 
-    public void Generate(GameObject[] detailPrefabs)
+    public void Generate()
     {
 
     }
 }
 
-public enum Direction { Null = -1, Forward, Back, Up, AtSide }
-public enum DetailType { Platfrom, Wheel, Cabin }
+public enum Direction { Null = -1, Forward, Back, Up, AtSide, length }
+public enum DetailType { Platfrom, Wheel, Cabin, length }
 
 public struct Probabilities
 {

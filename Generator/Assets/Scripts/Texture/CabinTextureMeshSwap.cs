9854a33dcebac3ad9ca5f72fa1251e92ea524a90
Buzzon <buzzondev@gmail.com>
2019-04-04 18:09:27 +0500

    ++

---
+++ Generator/Assets/Scripts/Texture/CabinTextureMeshSwap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CabinTextureMeshSwap : MonoBehaviour
+{
+    GameObject Mesh;
+    GameObject Mesh_low;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mesh = transform.GetChild(0).gameObject;
+        Mesh_low = transform.GetChild(1).gameObject;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
